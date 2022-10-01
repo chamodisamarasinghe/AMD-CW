5572f58454ee8bb16274ef2f4152eb33d14d6d9a
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-10-01 20:25:59 +0530

    Updated

--- screens/Register.js
+++ screens/Register.js
@@ -1,19 +1,47 @@
-import { View} from 'react-native'
+import {  View} from 'react-native'
 import React from 'react'
-import {  NativeBaseProvider, Box, Button, Switch, VStack, TextArea} from "native-base";
+import { NativeBaseProvider, Box,Text, VStack,Input,Icon,MaterialIcons,show,Pressable,Image, Center,Button} from "native-base";
+import Buttone from '../components/Buttone';
+import Colors from '../assets/color';
 
-
-export default function Register() {
+export default function Register({ navigation }) {
   return (
+      
+
     <NativeBaseProvider>
-    <VStack space={4} alignItems="center">
-      <Box>Hello world</Box>
-      <Button mt={'40%'} size="md" variant="outline" colorScheme="secondary" width={'80%'} onPress={()=>{navigation.navigate("AddData")}}>
-        Click To Add Data
-      </Button>
-      <Switch defaultIsChecked colorScheme="primary" size="lg" />
-      <TextArea h={20} placeholder="Text Area Placeholder" w="75%" maxW="300" />
-    </VStack>
-  </NativeBaseProvider>
+
+      
+    
+      <VStack space={3} w="75%" maxW="300px" mx="auto">
+      <Text fontSize="3xl" bold underline mt="10%" ml="34%" color={Colors.darkGreen}>Sign Up</Text>
+
+      <Center>
+      <Image 
+        source={require("../assets/log.png")}
+      />
+      </Center>
+
+
+      
+
+      <Input size="2xl" placeholder="First Name" mt="20%" />
+      <Input size="2xl" placeholder="Last Name" />
+      <Input size="2xl" placeholder="Address" />
+      <Input size="2xl" placeholder="User Name" />
+      <Input size="2xl" placeholder="Pass word" />
+
+      
+      <Button colorScheme="success" mt="20%" fontWeight="bold" rounded="full" >Sign Up</Button>
+
+      
+      </VStack>
+      
+
+      
+      </NativeBaseProvider>
+
+      
+      
+    
   )
 }
