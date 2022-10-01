115aa3107b1f03a0a44108c1640d764029306da0
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-10-01 15:46:46 +0530

    Updated

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Register from './screens/Register'
 import Login from './screens/Login'
+import AddData from './screens/AddData'
+import Home from './screens/Home'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -12,6 +14,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="AddData" component={AddData} />
       <Stack.Screen name="Register" component={Register} />
       
     </Stack.Navigator>
--- screens/Login.js
+++ screens/Login.js
@@ -2,17 +2,17 @@ import { View} from 'react-native'
 import React from 'react'
 import { NativeBaseProvider, Box,Text, VStack,Input,Icon,MaterialIcons,show,Pressable,Button } from "native-base";
 
-export default function Login() {
+export default function Login({ navigation }) {
   return (
     <NativeBaseProvider>
       
       <VStack space={10} w="75%" maxW="300px" mx="auto">
-      <Text fontSize="3xl" bold underline mt="10%" ml="35%" color="white">Login</Text>
+      <Text fontSize="3xl" bold underline mt="10%" ml="35%" color="black">Login</Text>
 
       <Input size="2xl" placeholder="User Name" mt="20%" />
-      <Input size="2xl" placeholder="User Name" />
+      <Input size="2xl" placeholder="Pass word" />
 
-      <Button mt="40%" onPress={() => console.log("hello world")}>Login</Button>
+      <Button mt="40%" onPress={()=>{navigation.navigate("AddData")}}>Login</Button>
       </VStack>
       
 
