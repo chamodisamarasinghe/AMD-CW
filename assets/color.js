0ad17aed2d5a4b31a7bbcf337c11829d36c4c7d7
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-10-01 19:28:41 +0530

    Updated File

---
+++ assets/color.js
@@ -0,0 +1,7 @@
+const Colors={
+    black:"#000000",
+    green: "#00FF00",
+    darkGreen: "#0A8A0A",
+};
+
+export default Colors;
--- screens/Login.js
+++ screens/Login.js
@@ -1,23 +1,43 @@
-import { View} from 'react-native'
+import {  View} from 'react-native'
 import React from 'react'
-import { NativeBaseProvider, Box,Text, VStack,Input,Icon,MaterialIcons,show,Pressable,Button } from "native-base";
+import { NativeBaseProvider, Box,Text, VStack,Input,Icon,MaterialIcons,show,Pressable,Image, Center,Button} from "native-base";
+import Buttone from '../components/Buttone';
+import Colors from '../assets/color';
 
 export default function Login({ navigation }) {
   return (
-    <NativeBaseProvider>
       
+
+    <NativeBaseProvider>
+
+      
+    
       <VStack space={10} w="75%" maxW="300px" mx="auto">
-      <Text fontSize="3xl" bold underline mt="10%" ml="35%" color="black">Login</Text>
+      <Text fontSize="3xl" bold underline mt="10%" ml="35%" color={Colors.darkGreen}>Login</Text>
+
+      <Center>
+      <Image 
+        source={require("../assets/log.png")}
+      />
+      </Center>
+
+
+      
 
       <Input size="2xl" placeholder="User Name" mt="20%" />
       <Input size="2xl" placeholder="Pass word" />
 
-      <Button mt="40%" onPress={()=>{navigation.navigate("AddData")}}>Login</Button>
+      
+      <Button colorScheme="success" mt="40%" fontWeight="bold" rounded="full" onPress={()=>{navigation.navigate("AddData")}}>Login</Button>
+
+      
       </VStack>
       
 
       
       </NativeBaseProvider>
+
+      
       
     
   )
