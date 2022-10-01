c78c128322f496681ede679ae803e97d7e4a77cd
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-10-01 13:33:22 +0530

    Updated files

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Register from './screens/Register'
+import Login from './screens/Login'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Register/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+      
+    </Stack.Navigator>
+  </NavigationContainer>
+
+      
+    
   )
 }
--- screens/Register.js
+++ screens/Register.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View} from 'react-native'
 import React from 'react'
+import {  NativeBaseProvider, Box, Button, Switch, VStack, TextArea} from "native-base";
+
 
 export default function Register() {
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <NativeBaseProvider>
+    <VStack space={4} alignItems="center">
+      <Box>Hello world</Box>
+      <Button mt={'40%'} size="md" variant="outline" colorScheme="secondary" width={'80%'} onPress={()=>{navigation.navigate("AddData")}}>
+        Click To Add Data
+      </Button>
+      <Switch defaultIsChecked colorScheme="primary" size="lg" />
+      <TextArea h={20} placeholder="Text Area Placeholder" w="75%" maxW="300" />
+    </VStack>
+  </NativeBaseProvider>
   )
 }
