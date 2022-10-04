a502904c539c424ad6bbf0d1fa29bf40889d414b
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-10-04 13:01:10 +0530

    Updated

--- screens/AddCar.js
+++ screens/AddCar.js
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
+import {  View} from 'react-native'
 import React from 'react'
+import { NativeBaseProvider, Box,Text, VStack,Input,Icon,MaterialIcons,show,Pressable,Image, Center,Button} from "native-base";
+import Buttone from '../components/Buttone';
+import Colors from '../assets/color';
 
-export default function AddCar() {
+export default function AddCar({ navigation }) {
   return (
-    <View>
-      <Text>AddCar</Text>
-    </View>
+      
+
+    <NativeBaseProvider>
+
+      
+    
+      <VStack space={3} w="75%" maxW="300px" mx="auto">
+      <Text fontSize="3xl" bold underline mt="10%" ml="18%" color={Colors.darkGreen}>Add a new Car</Text>
+
+      <Center>
+      <Image 
+        source={require("../assets/icons/car.png")}
+      />
+      </Center>
+
+
+      
+
+      <Input size="1xl" placeholder="Register Number" mt="20%" />
+      <Input size="1xl" placeholder="Brand" />
+      <Input size="1xl" placeholder="Vehicle Number" />
+      <Input size="1xl" placeholder="Price" />
+      {/* <Input size="1xl" placeholder="Pass word" /> */}
+
+      
+      <Button colorScheme="success" mt="20%" fontWeight="bold" rounded="full" onPress={()=>{navigation.navigate("CarDetails")}}>Add</Button>
+
+      
+      </VStack>
+      
+
+      
+      </NativeBaseProvider>
+
+      
+      
+    
   )
 }
