20843b563e4085e02debbe0b427294b45e23940d
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-10-05 20:52:15 +0530

    Updated

--- screens/AddCar.js
+++ screens/AddCar.js
@@ -55,26 +55,51 @@ import Colors from '../assets/color';
 
 
 export default function AddCar({ navigation }) {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [id, setId] = useState('');
+    const [registerNumber, setRegisterNumber] = useState('');
+    const [brand, setBrand] = useState('');
+    const [vehicleNumber, setVehicleNumber] = useState('');
+    const [price, setPrice] = useState('');
 
-    const saveData = () => {
-        fetch('https://jsonplaceholder.typicode.com/posts', {
+    saveCar = async () => {
+
+        if (registerNumber != "" && brand != "" && vehicleNumber != "" && price != "") {
+          fetch('http://192.168.1.100:8000/users', {
             method: 'POST',
             body: JSON.stringify({
-                title: title,
-                body: body,
-                userId: id,
+                  registerNumber: registerNumber,
+                    brand: brand,
+                    vehicleNumber: vehicleNumber,
+                    price: price
             }),
             headers: {
-                'Content-type': 'application/json; charset=UTF-8',
+              'Content-type': 'application/json; charset=UTF-8',															
             },
-        })
-            .then((response) => {Alert.alert("Save Saved Successfully !")})
-            .catch((err)=>{Alert.alert("Error occured !")})
-    }
+          })
+            .then((response) => response.json())
+            .then((json) => {
+              if (json.status === "500") {
+                Alert.alert(json.message);
+              } else {
+                Alert.alert(json.message);
+                clearTextFields();
+              }
+            })
+            .catch((err) => Alert.alert(err));
+        } else {
+          Alert.alert("Please fill all the fields and try again.")
+        }
+      }
+    
+      const clearTextFields = () => {
+        setRegisterNumber("");
+        setBrand("");
+        setVehicleNumber("");
+        setPrice("");
+      }
 
+
+
+      
     return (
         <NativeBaseProvider>
            <Text fontSize="3xl" bold underline mt="10%" ml="20%" color={Colors.darkGreen}>Add a new Car</Text>
@@ -87,15 +112,15 @@ export default function AddCar({ navigation }) {
 
 
             <VStack space={4}  mt="15%"   maxW="300px" mx="auto">
-                <Input mx="3" value={title} color="black" onChangeText={(e) => { setTitle(e) }} placeholder="Register Number" w="80%" />
-                <Input mx="3" value={body} color="black" onChangeText={(e) => { setBody(e) }} placeholder="Brand" w="80%" />
-                <Input mx="3" value={id} color="black" onChangeText={(e) => { setId(e) }} placeholder="Vehicle Number" w="80%" />
-                <Input mx="3" value={id} color="black" onChangeText={(e) => { setId(e) }} placeholder="Price" w="80%" />
-                <Button size="md"  colorScheme="primary" rounded="full" onPress={saveData}>
+                <Input mx="3" value={registerNumber} color="black" onChangeText={(e) => { setRegisterNumber(e) }} placeholder="Register Number" w="80%" />
+                <Input mx="3" value={brand} color="black" onChangeText={(e) => { setBrand(e) }} placeholder="Brand" w="80%" />
+                <Input mx="3" value={vehicleNumber} color="black" onChangeText={(e) => { setVehicleNumber(e) }} placeholder="Vehicle Number" w="80%" />
+                <Input mx="3" value={price} color="black" onChangeText={(e) => { setPrice(e) }} placeholder="Price" w="80%" />
+                <Button size="md"  colorScheme="primary" rounded="full" onPress={saveCar}>
                     Save Post
                 </Button>
 
-                <Button colorScheme="success" mt="20%" fontWeight="bold" rounded="full" onPress={()=>{navigation.navigate("CarDetails")}}>Car Details</Button>
+               
             </VStack>
         </NativeBaseProvider>
     )
--- screens/Register.js
+++ screens/Register.js
@@ -1,10 +1,49 @@
 import {  View} from 'react-native'
-import React from 'react'
+import React ,{ useState } from 'react'
 import { NativeBaseProvider, Box,Text, VStack,Input,Icon,MaterialIcons,show,Pressable,Image, Center,Button} from "native-base";
 import Buttone from '../components/Buttone';
 import Colors from '../assets/color';
 
 export default function Register({ navigation }) {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contactNum, setContactNum] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  saveUser = async () => {
+
+    if (fullName != "" && email != "" && contactNum != "" && username != "" && password != "") {
+      fetch('http://192.168.1.100:8000/users', {
+        method: 'POST',
+        body: JSON.stringify({
+          	    fullName: fullName,
+                email: email,
+                contactNum: contactNum,
+                username: username,
+		password: password
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',															
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          if (json.status === "500") {
+            Alert.alert(json.message);
+          } else {
+            Alert.alert(json.message);
+            clearTextFields();
+          }
+        })
+        .catch((err) => Alert.alert(err));
+    } else {
+      Alert.alert("Please fill all the fields and try again.")
+    }
+  }
+
+
   return (
       
 
@@ -22,16 +61,27 @@ export default function Register({ navigation }) {
       </Center>
 
 
-      
+      <VStack space={4}  mt="15%"   maxW="300px" mx="auto">
+                <Input mx="3" value={fullName} color="black" onChangeText={(e) => { setFullName(e) }} placeholder="Full Name" w="80%" />
+                <Input mx="3" value={email} color="black" onChangeText={(e) => { setEmail(e) }} placeholder="Email" w="80%" />
+                <Input mx="3" value={contactNum} color="black" onChangeText={(e) => { setContactNum(e) }} placeholder="Contact Num" w="80%" />
+                <Input mx="3" value={username} color="black" onChangeText={(e) => { setUsername(e) }} placeholder="Username" w="80%" />
+                <Input mx="3" value={password} color="black" onChangeText={(e) => { setPassword(e) }} placeholder="Password" w="80%" />
+                <Button size="md"  colorScheme="primary" rounded="full" onPress={saveUser}>
+                    Save Car
+                </Button>
 
-      <Input size="2xl" placeholder="First Name" mt="20%" />
-      <Input size="2xl" placeholder="Last Name" />
-      <Input size="2xl" placeholder="Address" />
+                <Button colorScheme="success" mt="20%" fontWeight="bold" rounded="full" onPress={()=>{navigation.navigate("CarDetails")}}>Car Details</Button>
+            </VStack>
+
+      {/* <Input size="2xl" placeholder="Full Name" mt="20%" />
+      <Input size="2xl" placeholder="Email" />
+      <Input size="2xl" placeholder="Contact" />
       <Input size="2xl" placeholder="User Name" />
       <Input size="2xl" placeholder="Pass word" />
 
       
-      <Button colorScheme="success" mt="20%" fontWeight="bold" rounded="full" >Sign Up</Button>
+      <Button colorScheme="success" mt="20%" fontWeight="bold" rounded="full" >Sign Up</Button> */}
 
       
       </VStack>
